407d9cdab14a0fc1883129f8412dceaa896a2a8f
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2020-10-20 18:09:04 -0300

    iniciando pagina de login

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,20 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Home from '@/pages/Home'
+import Login from '@/pages/Login'
 
 Vue.use(Router)
 
 const routes = [
   {
     name: 'home',
-    path:'/',
+    path: '/',
     component: Home,
+  },
+  {
+    name: 'login',
+    path: '/login',
+    component: Login
   }
 ]
 
