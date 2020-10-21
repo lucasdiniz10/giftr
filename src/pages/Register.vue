8c7c7ee6b29ceb4cbf6e40d6c66ad3c2287501f8
dannilo <dannilo-pires@hotmail.com>
2020-10-21 13:52:33 -0300

    alterando page login e register

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,3 +1,35 @@
 <template>
-    <h1>cadastro</h1>
+  <div class="container" id="login">
+    <header class="header-container">
+      <h1 id="title">Obrigado por dar esse passo!</h1>
+      <p>Bem vindo ao seu cadastro.</p>
+    </header>
+    <main class="main-container">
+      <Form />
+    </main>
+  </div>
 </template>
+
+<script>
+import Form from '../components/Form'
+
+export default {
+  name: 'Register',
+
+  components: {
+    Form,
+  },
+}
+</script>
+
+<style lang='stylus' scoped>
+  
+  header {
+    text-align center
+    margin-bottom 3.2rem
+  }
+  
+  header #title {
+    font: 800 3.2rem Montserrat;
+  }
+</style>
