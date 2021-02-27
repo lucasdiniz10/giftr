294c19a99cb7b4094f1034b3cd95ed9c8170655a
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2021-02-27 02:56:09 -0300

    deploy1

---
+++ dist/js/chunk-2d0ba09d.17ca1162.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0ba09d"],{3604:function(n,u,l){"use strict";l.r(u);var a,c,e=l("2877"),o={},p=Object(e["a"])(o,a,c,!1,null,null,null);u["default"]=p.exports}}]);
+//# sourceMappingURL=chunk-2d0ba09d.17ca1162.js.map
---
+++ dist/js/chunk-2d21d885.3e7e6213.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21d885"],{d267:function(n,u,l){"use strict";l.r(u);var c,d,e=l("2877"),o={},p=Object(e["a"])(o,c,d,!1,null,null,null);u["default"]=p.exports}}]);
+//# sourceMappingURL=chunk-2d21d885.3e7e6213.js.map
